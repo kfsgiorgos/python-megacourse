244701fa2c1fdc9ffef365aa3dfbe9b615eedd4a
Georgios Kaiafas <kfsgiorgos@gmail.com>
2022-01-25 17:04:07 +0100

    cheatsheet & more exercises

--- file_processing.py
+++ file_processing.py
@@ -19,6 +19,27 @@ with open("bear_copy.txt", "w") as file:
     file.write(content[:90])
 
 
+# append the text of bear1.txt to bear2.txt 
+with open("bear1.txt", "r") as file:
+    content = file.read()
+with open("bear2.txt", "a") as file:
+    file.write('\n')
+    file.write(content)
+
+
+
+#modify the content of data.txt in order to have two times its content
+
+# Hint: Create a "with" block where you open the file in 'a+' mode. Put the cursor on top of the file. 
+# Read the file storing its content in a variable, put the cursor on top of the file, write the content, 
+# write the content.
+with open("data.txt", "a+") as file:
+    file.seek(0)
+    content = file.read()
+    file.seek(0)
+    file.write(content)
+    file.write(content)
+
 
 
 
