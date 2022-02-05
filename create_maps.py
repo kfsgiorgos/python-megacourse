83ac2027fdd340e44de9bc45ae5a4f88be9bc8d4
Georgios Kaiafas <kfsgiorgos@gmail.com>
2022-02-05 13:38:42 +0100

    markers with different colour based on elevation

--- create_maps.py
+++ create_maps.py
@@ -7,11 +7,19 @@ lat = list(data.LAT)
 lon = list(data.LON)
 elev = list(data.ELEV)
 
+def colour_producer(elevation):
+    if elevation < 1000:
+        return "green"
+    elif 1000 <= elevation < 2500:
+        return "orange"
+    else:
+        return "red" 
+
 map = folium.Map(location=[38.58, -99.09], zoom_start = 6, tiles = "Stamen Terrain")
 fg = folium.FeatureGroup(name=" US map with Volanoes")
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup="Elevation is: "+str(el/1000)+ "km", icon=folium.Icon(color="green")))
+    fg.add_child(folium.Marker(location=[lt, ln], popup="Elevation is: "+str(el)+ "m", icon=folium.Icon(color=colour_producer(el))))
 
 map.add_child(fg)
 map.save("/Users/georgios.kaiafas/Documents/GitHub/python-megacourse/Map1.html")
